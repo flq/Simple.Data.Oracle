f40f9a9e6c57aea9bab5fd8efabdb07e44746249
unknown <simon@slimtech-it.co.uk>
2012-08-21 18:47:34 +0100

    update of nuget package to 0.17.0.0

--- Simple.Data.Oracle/Properties/AssemblyInfo.cs
+++ Simple.Data.Oracle/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Simple.Data.Oracle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Frank L. Quednau")]
+[assembly: AssemblyCompany("Simon Hanlon")]
 [assembly: AssemblyProduct("Simple.Data.Oracle")]
 [assembly: AssemblyCopyright("Copyright © realfiction 2012")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.14.0.0")]
-[assembly: AssemblyFileVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.17.0.0")]
+[assembly: AssemblyFileVersion("0.17.0.0")]
 
 [assembly: InternalsVisibleTo("Simple.Data.Oracle.Tests")]
